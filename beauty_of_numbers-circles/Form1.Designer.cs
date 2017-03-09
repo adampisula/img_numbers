b6edf9e4158db51b5276794abd3115cb312a1090
Adam Pisula <adam.pisula@outlook.com>
2017-03-09 21:02:32 +0100

    img_numbers

---
+++ beauty_of_numbers-circles/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace beauty_of_numbers_circles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(122, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(122, 38);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(122, 64);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown3.TabIndex = 2;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(122, 90);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Średnica";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Odstep";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Odstęp od ściany";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Szerokość";
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(122, 116);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown5.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Wysokość";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 168);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(197, 138);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "Liczba...";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(172, 317);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "0/0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "Text...";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(51, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 344);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ beauty_of_numbers-circles/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace beauty_of_numbers_circles
+{
+    public partial class Form1 : Form
+    {
+        static int width, height, span, spanb, s;
+        static int x, y, sx, sy, qx, qy, times;
+        StreamReader reader;
+        DateTime prev, now;
+        TimeSpan one, approx;
+        int app;
+
+        Form2 form2;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            reader = new StreamReader(myStream);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            label6.Text = richTextBox1.Text.Length.ToString() + '/' + ((Convert.ToInt32(numericUpDown4.Value) - (2 * Convert.ToInt32(numericUpDown3.Value))) / (Convert.ToInt32(numericUpDown1.Value) + Convert.ToInt32(numericUpDown2.Value)) + 1) * ((Convert.ToInt32(numericUpDown5.Value) - (2 * Convert.ToInt32(numericUpDown3.Value))) / (Convert.ToInt32(numericUpDown1.Value) + Convert.ToInt32(numericUpDown2.Value)) + 1);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        public Graphics g;
+        //char[] number;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s = Convert.ToInt32(numericUpDown1.Value);
+            span = Convert.ToInt32(numericUpDown2.Value);
+            spanb = Convert.ToInt32(numericUpDown3.Value);
+            width = Convert.ToInt32(numericUpDown4.Value);
+            height = Convert.ToInt32(numericUpDown5.Value);
+
+            qx = (width - (2 * spanb)) / (s + span) + 1;
+            qy = (height - (2 * spanb)) / (s + span) + 1;
+
+            //if (textBox1.Text.Length != 137)
+            //    MessageBox.Show("Ilość znaków musi być równa 137, a jest równa " + textBox1.Text.Length, "Za mało/dużo znaków!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            //else if (richTextBox1.Text.Length < qx * qy)
+                //MessageBox.Show("Ilość znaków musi być większa lub równa x * y", "Za mało znaków!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            //else
+            //{
+                //number = new char[richTextBox1.Text.Length + 1];
+
+                //for (int i = 0; i < richTextBox1.Text.Length; i++)
+                    //number[i] = richTextBox1.Text[i];
+
+                //number[richTextBox1.Text.Length] = '#';
+
+                SystemSounds.Beep.Play();
+
+                form2 = new beauty_of_numbers_circles.Form2();
+
+                if (width == SystemInformation.VirtualScreen.Width && height == SystemInformation.VirtualScreen.Height)
+                {
+                    form2.FormBorderStyle = FormBorderStyle.None;
+                    form2.WindowState = FormWindowState.Maximized;
+                    form2.TopMost = true;
+                }
+                form2.Width = Convert.ToInt32(numericUpDown4.Value + 15);
+                form2.Height = Convert.ToInt32(numericUpDown5.Value + 38);
+                form2.BackColor = Color.Black;
+                form2.Show();
+
+                g = form2.CreateGraphics();
+
+                times = 0;
+                x = spanb;
+                y = spanb;
+
+                for (int i = 0; i < qy; i++)
+                {
+                    prev = DateTime.Now;
+
+                    for (int j = 0; j < qx; j++)
+                    {
+                        draw_rect(x, y, s, richTextBox1.Text[times]);
+                        x += span + s;
+                        times++;
+
+                        if (times == richTextBox1.Text.Length - 1)
+                            break;
+                    }
+                    now = DateTime.Now;
+                    one = now - prev;
+                    approx = new TimeSpan(one.Ticks * (qy - (times / qx + 1)));
+
+                    g.FillRectangle(new SolidBrush(Color.Black), spanb, height - spanb / 2 - 10, 300, 100);
+                    g.DrawString(approx.Hours.ToString() + ':' + approx.Minutes.ToString() + ':' + approx.Seconds.ToString(), new Font("Courier new", 12), new SolidBrush(Color.White), spanb, height - spanb / 2 - 10);
+
+                    if (times == richTextBox1.Text.Length - 1)
+                        break;
+                    x = spanb;
+                    y += span + s;
+                }
+
+                SystemSounds.Beep.Play();
+
+                x = spanb + s / 2;
+                y = spanb + s / 2;
+                times = 0;
+
+                for (int i = 0; i < qy; i++)
+                {
+                    prev = DateTime.Now;
+                    for (int j = 0; j < qx; j++)
+                    {
+                        draw_lines(x, y, times, qx);
+                        x += span + s;
+                        times++;
+
+                        if (times == richTextBox1.Text.Length - 1)
+                            break;
+                    }
+                    now = DateTime.Now;
+                    one = now - prev;
+                    approx = new TimeSpan(one.Ticks * (qy - (times / qx + 1)));
+
+                    g.FillRectangle(new SolidBrush(Color.Black), spanb, height - spanb / 2 - 10, 300, 100);
+                    g.DrawString(approx.Hours.ToString() + ':' + approx.Minutes.ToString() + ':' + approx.Seconds.ToString(), new Font("Courier new", 12), new SolidBrush(Color.White), spanb, height - spanb / 2 - 10);
+
+                    if (times == richTextBox1.Text.Length - 1)
+                        break;
+                    x = spanb + s / 2;
+                    y += span + s;
+                }
+
+                SystemSounds.Beep.Play();
+
+                g.FillRectangle(new SolidBrush(Color.Black), spanb, height - spanb / 2 - 10, 300, 100);
+                g.DrawString(textBox1.Text, new Font("Courier new", 16), new SolidBrush(Color.White), spanb + 5, spanb / 2 - 15);
+                g.DrawString("Adam Pisula 2016", new Font("Courier new", 16), new SolidBrush(Color.White), (width - spanb - 220), (height - spanb / 2 - 15));
+
+                this.Close();
+
+                Thread.Sleep(15000);
+
+                g.Dispose();
+                form2.FormBorderStyle = FormBorderStyle.Sizable;
+                form2.WindowState = FormWindowState.Normal;
+                form2.TopMost = false;
+            //}
+        }
+
+        private Color choose_color(char number)
+        {
+            Color col;
+
+            switch (number)
+            {
+                case '0':
+                    col = Color.White;
+                    break;
+
+                case '1':
+                    col = ColorTranslator.FromHtml("#EA0037");
+                    break;
+
+                case '2':
+                    col = ColorTranslator.FromHtml("#FF7100");
+                    break;
+
+                case '3':
+                    col = ColorTranslator.FromHtml("#FFDE00");
+                    break;
+
+                case '4':
+                    col = ColorTranslator.FromHtml("#71E500");
+                    break;
+
+                case '5':
+                    col = ColorTranslator.FromHtml("#00A57D");
+                    break;
+
+                case '6':
+                    col = ColorTranslator.FromHtml("#028D9B");
+                    break;
+
+                case '7':
+                    col = ColorTranslator.FromHtml("#001979");
+                    break;
+
+                case '8':
+                    col = ColorTranslator.FromHtml("#7209AA");
+                    break;
+
+                case '9':
+                    col = ColorTranslator.FromHtml("#C50080");
+                    break;
+                default:
+                    col = Color.Black;
+                    break;
+            }
+
+            return col;
+        }
+
+        private void draw_rect(int x, int y, int size, char number) 
+        {
+            SolidBrush sb = new SolidBrush(choose_color(number));
+
+            g.FillRectangle(sb, x, y, size, size);
+        }
+
+        private void draw_lines(int x, int y, int times, int qx)
+        {
+            Pen pen = new Pen(choose_color(richTextBox1.Text[times]), s / 4);
+
+            if (times == 0)
+            {
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + 1])         g.DrawLine(pen, x, y, x + (span + s), y);
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx])        g.DrawLine(pen, x, y, x, y + (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx + 1])    g.DrawLine(pen, x, y, x + (span + s), y + (span + s));
+            }
+
+            else if (times > 0 && times < qx)
+            {
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - 1])         g.DrawLine(pen, x, y, x - (span + s), y);
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx - 1])    g.DrawLine(pen, x, y, x - (span + s), y + (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx])        g.DrawLine(pen, x, y, x, y + (span + s));
+            }
+
+            else if (times == qx - 1)
+            {
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - 1])         g.DrawLine(pen, x, y, x - (span + s), y);
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx - 1])    g.DrawLine(pen, x, y, x - (span + s), y + (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx])        g.DrawLine(pen, x, y, x, y + (span + s));
+            }
+
+            else if (times % qx == 0 && times != qx * qy - qx)
+            {
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx])        g.DrawLine(pen, x, y, x, y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx + 1])    g.DrawLine(pen, x, y, x + (span + s), y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + 1])         g.DrawLine(pen, x, y, x + (span + s), y);
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx])        g.DrawLine(pen, x, y, x, y + (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx + 1])    g.DrawLine(pen, x, y, x + (span + s), y + (span + s));
+            }
+
+            else if ((times + 1) % qx == 0 && times != qx * qy - 1)
+            {
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx - 1])    g.DrawLine(pen, x, y, x - (span + s), y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx])        g.DrawLine(pen, x, y, x, y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - 1])         g.DrawLine(pen, x, y, x - (span + s), y);
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx - 1])    g.DrawLine(pen, x, y, x - (span + s), y + (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx])        g.DrawLine(pen, x, y, x, y + (span + s));
+            }
+
+            else if (times == qx * qy - qx)
+            {
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx])        g.DrawLine(pen, x, y, x, y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx + 1])    g.DrawLine(pen, x, y, x + (span + s), y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + 1])         g.DrawLine(pen, x, y, x + (span + s), y);
+            }
+
+            else if (times > qx * qy - qx && times < qx * qy - 1)
+            {
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx - 1])    g.DrawLine(pen, x, y, x - (span + s), y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx])        g.DrawLine(pen, x, y, x, y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx + 1])    g.DrawLine(pen, x, y, x + (span + s), y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - 1])         g.DrawLine(pen, x, y, x - (span + s), y);
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + 1])         g.DrawLine(pen, x, y, x + (span + s), y);
+            }
+
+            else if (times == qx * qy - 1)
+            {
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx - 1])    g.DrawLine(pen, x, y, x - (span + s), y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx])        g.DrawLine(pen, x, y, x, y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - 1])         g.DrawLine(pen, x, y, x - (span + s), y);
+            }
+
+            else
+            {
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx - 1])    g.DrawLine(pen, x, y, x - (span + s), y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx])        g.DrawLine(pen, x, y, x, y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - qx + 1])    g.DrawLine(pen, x, y, x + (span + s), y - (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times - 1])         g.DrawLine(pen, x, y, x - (span + s), y);
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + 1])         g.DrawLine(pen, x, y, x + (span + s), y);
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx - 1])    g.DrawLine(pen, x, y, x - (span + s), y + (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx])        g.DrawLine(pen, x, y, x, y + (span + s));
+                if (richTextBox1.Text[times] == richTextBox1.Text[times + qx + 1])    g.DrawLine(pen, x, y, x + (span + s), y + (span + s));
+            }
+        }
+    }
+}
